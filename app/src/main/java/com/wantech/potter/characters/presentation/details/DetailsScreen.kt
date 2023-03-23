bfabf6ff68fece18097733d014febde918e8ec50
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-23 11:51:33 +0300

    Retrofit PotterApi SetUp

--- app/src/main/java/com/wantech/potter/characters/presentation/details/DetailsScreen.kt
+++ app/src/main/java/com/wantech/potter/characters/presentation/details/DetailsScreen.kt
@@ -1,19 +1,21 @@
 package com.wantech.potter.characters.presentation.details
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 
 @Composable
-fun DetailsScreen(navController:NavHostController,itemId:String) {
+fun DetailsScreen(itemId: String) {
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "The Id $itemId")
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center
+            ) {
+
+            }
         }
     }
 }
